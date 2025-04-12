4c1c9ab2c7a21afe64f2b8332d359b6793779f37
AdrianRadev-cmg <adriyanradev@gmail.com>
2025-04-12 15:15:44 +0100

    Initial Commit

---
+++ Tilo/TiloApp.swift
@@ -0,0 +1,17 @@
+//
+//  TiloApp.swift
+//  Tilo
+//
+//  Created by Adrian Radev on 12/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct TiloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
